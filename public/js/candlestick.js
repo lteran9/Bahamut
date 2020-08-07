536e21eb5bf27736d2591e39ee60f983bc75d2a2
diegyves2 <diegyves2@gmail.com>
2020-08-06 18:38:51 -0700

    added candlestick class

---
+++ public/js/candlestick.js
@@ -0,0 +1,17 @@
+/**
+ * Handles the code necessary to compute Candlesticks.
+ */
+class Candlestick {
+
+    /**
+     * 
+     * @param {*} 
+     */
+    constructor() {
+        
+      
+    }
+ 
+   
+ }
+ 
