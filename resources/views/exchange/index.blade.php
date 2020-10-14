7c768607d0033ee572d05643d8154e3957ad3beb
LT <support@teran.tech>
2020-10-13 20:51:35 -0700

    laravel     - Added more coins (ETH & XRP) to exchange analyzer

--- resources/views/exchange/index.blade.php
+++ resources/views/exchange/index.blade.php
@@ -1,20 +1,38 @@
 @extends('layouts.app')
 @section('content')
 <div class="container my-5">
-   <h1>Exchange</h1>
-   <hr />
-   <div class="bhm-exchange">
-      <div class="row">
-         <div class="col-md-4">
-            <div class="card">
-               <div class="card-body">
-                  <a href="{{route('exchange.coin', ['coin' => 'BTC-USD'])}}" class="product-link">
-                     <h2 class="text-center">BTC-USD</h2>
-                  </a>
-               </div>
+    <h1>Exchange</h1>
+    <hr />
+    <div class="bhm-exchange">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="{{route('exchange.coin', ['coin' => 'BTC-USD'])}}" class="product-link">
+                            <h2 class="text-center">BTC-USD</h2>
+                        </a>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="{{route('exchange.coin', ['coin' => 'ETH-USD'])}}" class="product-link">
+                            <h2 class="text-center">ETH-USD</h2>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <a href="{{route('exchange.coin', ['coin' => 'XRP-USD'])}}" class="product-link">
+                            <h2 class="text-center">XRP-USD</h2>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
