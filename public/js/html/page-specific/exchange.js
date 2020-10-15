34786878cba4d22e306029b421d522150c35d434
LT <support@teran.tech>
2020-10-14 22:39:17 -0700

    laravel     - Working on buy & sell indicators

--- public/js/html/page-specific/exchange.js
+++ public/js/html/page-specific/exchange.js
@@ -49,164 +49,164 @@ var exchange = (function () {
     function updateUI(periods) {
         function updateEMAs(shortAvg, longAvg, container) {
             var shortElement = $(container).find('[data-id="ema12"]'),
-                longElement = $(container).find('[data-id="ema26"]');
+                longElement = $(container).find('[data-id="ema26"]'),
+                buyIndicators = $(container).find('[data-id="buyIndicators"]')[0],
+                sellIndicators = $(container).find('[data-id="sellIndicators"]')[0];
 
             if (shortAvg != '-') {
                 $(shortElement).html(shortAvg.toFixed(4));
-
-                if (shortAvg > longAvg) {
-                    $(shortElement).addClass('on');
-                } else {
-                    if ($(shortElement).hasClass('on'))
-                        $(shortElement).removeClass('on');
-                }
-            }
-
-            if (longAvg != '-') {
-                $(longElement).html(longAvg.toFixed(4));
-
-                if (shortAvg < longAvg) {
-                    $(longElement).addClass('on');
-                } else {
-                    if ($(longElement).hasClass('on'))
-                        $(longElement).removeClass('on');
-                }
+                $(shortElement).addClass('on');
+            } else {
+                if ($(shortElement).hasClass('on'))
+                    $(shortElement).removeClass('on');
             }
         }
 
-        function updateProgressBars(period, container) {
-            if (period.movingAverages.length <= 12) {
-                var progressBar = $(container).find('#' + period.period + '-ema12');
-                if (progressBar) {
-                    var width = (period.movingAverages.length / 12) * 100;
-                    $(progressBar).css('width', width + '%');
-                }
+        if (longAvg != '-') {
+            $(longElement).html(longAvg.toFixed(4));
+
+            if (shortAvg < longAvg) {
+                $(longElement).addClass('on');
             } else {
-                $(container).find('#' + period.period + '-ema12').parent().remove();
-            }
-
-            if (period.movingAverages.length <= 26) {
-                var progressBar = $(container).find('#' + period.period + '-ema26');
-                if (progressBar) {
-                    var width = (period.movingAverages.length / 26) * 100;
-                    $(progressBar).css('width', width + '%');
-                }
-            } else {
-                $(container).find('#' + period.period + '-ema26').parent().remove();
-            }
-        }
-
-        for (var i = 0; i < periods.length; i++) {
-            var shortAvg = periods[i].shortAvg,
-                longAvg = periods[i].longAvg;
-
-            var container = $(elements.averages).find('#' + periods[i].period);
-            if (container) {
-                updateEMAs(shortAvg, longAvg, container);
-                updateProgressBars(periods[i], container);
+                if ($(longElement).hasClass('on'))
+                    $(longElement).removeClass('on');
             }
         }
     }
+
+    function updateProgressBars(period, container) {
+        if (period.movingAverages.length <= 12) {
+            var progressBar = $(container).find('#' + period.period + '-ema12');
+            if (progressBar) {
+                var width = (period.movingAverages.length / 12) * 100;
+                $(progressBar).css('width', width + '%');
+            }
+        } else {
+            $(container).find('#' + period.period + '-ema12').parent().remove();
+        }
+
+        if (period.movingAverages.length <= 26) {
+            var progressBar = $(container).find('#' + period.period + '-ema26');
+            if (progressBar) {
+                var width = (period.movingAverages.length / 26) * 100;
+                $(progressBar).css('width', width + '%');
+            }
+        } else {
+            $(container).find('#' + period.period + '-ema26').parent().remove();
+        }
+    }
+
+    for (var i = 0; i < periods.length; i++) {
+        var shortAvg = periods[i].shortAvg,
+            longAvg = periods[i].longAvg;
+
+        var container = $(elements.averages).find('#' + periods[i].period);
+        if (container) {
+            updateEMAs(shortAvg, longAvg, container);
+            updateProgressBars(periods[i], container);
+        }
+    }
+}
 
     function updatePrice(price) {
-        var element = $(elements.container).find('[data-id="current-price"]');
-        if (element)
-            $(element).html('$' + price);
+    var element = $(elements.container).find('[data-id="current-price"]');
+    if (element)
+        $(element).html('$' + price);
+}
+
+function updateTimer(seconds) {
+    $(elements.clock).html(seconds);
+}
+
+function messageReceived(msg) {
+    function filter(update) {
+        // decide if the update should be shown or not - we only handler ticker updates.
+        update.time = update.time || new Date().toISOString();
+        update.last_size = update.last_size || 0.0;
+
+        return update.type === 'ticker' && update.price > 0;
     }
 
-    function updateTimer(seconds) {
-        $(elements.clock).html(seconds);
+    function ajaxSend(data) {
+        $.ajax({
+            url: '/exchange/tick',
+            method: 'post',
+            data: data,
+            error: function () {
+                console.log('error sending data');
+            },
+            complete: function () {
+                //console.log('message sent');
+            }
+        });
     }
 
-    function messageReceived(msg) {
-        function filter(update) {
-            // decide if the update should be shown or not - we only handler ticker updates.
-            update.time = update.time || new Date().toISOString();
-            update.last_size = update.last_size || 0.0;
+    var ticker = JSON.parse(msg);
 
-            return update.type === 'ticker' && update.price > 0;
+    ticker.rawtime = ticker.time;
+    ticker.time = new Date(Date.parse(ticker.time));
+
+    // Only get data we are intersted in
+    if (filter(ticker)) {
+        var data = {
+            trade_id: ticker.trade_id,
+            epoch: ticker.rawtime,
+            datetime: ticker.time,
+            product_id: ticker.product_id,
+            size: ticker.last_size,
+            price: ticker.price,
+            side: ticker.side,
+            sequence: ticker.sequence
         }
 
-        function ajaxSend(data) {
-            $.ajax({
-                url: '/exchange/tick',
-                method: 'post',
-                data: data,
-                error: function () {
-                    console.log('error sending data');
-                },
-                complete: function () {
-                    //console.log('message sent');
-                }
+        // Send data
+        //ajaxSend(data);
+
+        if (curatedDataset.length == 0 || parseInt((ticker.time.getTime() - curatedDataset[0].datetime.getTime()) / 1000) >= 5) {
+            // Store data
+            curatedDataset.unshift(data);
+
+            this.ema.update(curatedDataset);
+            updateUI(this.ema.getPeriods());
+        }
+
+        updatePrice(ticker.price);
+        rawTransactions.unshift(ticker);
+    }
+
+    if (rawTransactions.length > 52) {
+        rawTransactions.pop();
+    }
+
+    if (curatedDataset.length > 52) {
+        curatedDataset.pop();
+    }
+}
+
+function initCoinViews() {
+    function feedStart() {
+        if (websockets.length > 0) {
+            websockets.forEach(function (item, index) {
+                item.feed.start(buffer);
             });
         }
-
-        var ticker = JSON.parse(msg);
-
-        ticker.rawtime = ticker.time;
-        ticker.time = new Date(Date.parse(ticker.time));
-
-        // Only get data we are intersted in
-        if (filter(ticker)) {
-            var data = {
-                trade_id: ticker.trade_id,
-                epoch: ticker.rawtime,
-                datetime: ticker.time,
-                product_id: ticker.product_id,
-                size: ticker.last_size,
-                price: ticker.price,
-                side: ticker.side,
-                sequence: ticker.sequence
-            }
-
-            // Send data
-            //ajaxSend(data);
-
-            if (curatedDataset.length == 0 || parseInt((ticker.time.getTime() - curatedDataset[0].datetime.getTime()) / 1000) >= 5) {
-                // Store data
-                curatedDataset.unshift(data);
-
-                this.ema.update(curatedDataset);
-                updateUI(this.ema.getPeriods());
-            }
-
-            updatePrice(ticker.price);
-            rawTransactions.unshift(ticker);
-        }
-
-        if (rawTransactions.length > 52) {
-            rawTransactions.pop();
-        }
-
-        if (curatedDataset.length > 52) {
-            curatedDataset.pop();
-        }
     }
 
-    function initCoinViews() {
-        function feedStart() {
-            if (websockets.length > 0) {
-                websockets.forEach(function (item, index) {
-                    item.feed.start(buffer);
-                });
-            }
-        }
+    // Add websocket to elements
+    var product = document.getElementById('product');
+    websockets.push({
+        feed: new Feed(product.innerText, messageReceived)
+    });
 
-        // Add websocket to elements
-        var product = document.getElementById('product');
-        websockets.push({
-            feed: new Feed(product.innerText, messageReceived)
-        });
+    feedStart();
 
-        feedStart();
+    this.ema = new EMA(product);
+    this.clock = new Timer(updateTimer);
+}
 
-        this.ema = new EMA(product);
-        this.clock = new Timer(updateTimer);
+return {
+    init: function () {
+        initCoinViews();
     }
-
-    return {
-        init: function () {
-            initCoinViews();
-        }
-    }
-})();
+}
+}) ();
