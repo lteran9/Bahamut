464945961b1aa9cf7477c1b5237029553db83b82
LT <support@teran.tech>
2021-01-15 12:34:41 -0700

    laravel     - Added new coins to exchange dashboard

--- resources/views/exchange/index.blade.php
+++ resources/views/exchange/index.blade.php
@@ -6,7 +6,7 @@
     <div class="bhm-exchange">
         <div class="row">
             <div class="col-md-4">
-                <div class="card">
+                <div class="card mb-4">
                     <div class="card-body">
                         <a href="{{route('exchange.coin', ['coin' => 'BTC-USD'])}}" class="product-link">
                             <h2 class="text-center">BTC-USD</h2>
@@ -15,7 +15,7 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card mb-4">
                     <div class="card-body">
                         <a href="{{route('exchange.coin', ['coin' => 'ETH-USD'])}}" class="product-link">
                             <h2 class="text-center">ETH-USD</h2>
@@ -24,10 +24,37 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="card">
+                <div class="card mb-4">
                     <div class="card-body">
                         <a href="{{route('exchange.coin', ['coin' => 'LTC-USD'])}}" class="product-link">
                             <h2 class="text-center">LTC-USD</h2>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <a href="{{route('exchange.coin', ['coin' => 'LINK-USD'])}}" class="product-link">
+                            <h2 class="text-center">LINK-USD</h2>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <a href="{{route('exchange.coin', ['coin' => 'OMG-USD'])}}" class="product-link">
+                            <h2 class="text-center">OMG-USD</h2>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <a href="{{route('exchange.coin', ['coin' => 'GRT-USD'])}}" class="product-link">
+                            <h2 class="text-center">GRT-USD</h2>
                         </a>
                     </div>
                 </div>
