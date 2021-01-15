c15cba7d422da211f7b802be130888967cf05e9e
LT <support@teran.tech>
2021-01-15 12:27:34 -0700

    laravel     - Updated size of Products card     - Displaying more products on list page

--- resources/views/products/list.blade.php
+++ resources/views/products/list.blade.php
@@ -19,41 +19,45 @@
 
     @if (isset($products) && count($products) > 0)
     <div class="container bhm-container">
-        @foreach($products as $product)
-        <div class="card mb-4" data-filter="{{$product->display_name}}">
-            <div class="card-body">
-                <h4 class="card-title">
-                    {{$product->display_name}}
-                </h4>
-                <div class="row">
-                    <div class="col-12">
-                        <ul class="bhm-list">
-                            <li>
-                                <a href="{{route('products.history', ['id' => $product->id])}}">
-                                    Trade History
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{route('products.order-book', ['id' => $product->id])}}">
-                                    Order Book
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#!">
-                                    Ticker
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{route('products.stats', ['id' => $product->id])}}">
-                                    24hr Stats
-                                </a>
-                            </li>
-                        </ul>
+        <div class="row">
+            @foreach($products as $product)
+            <div class="col-12 col-md-2" data-filter="{{$product->display_name}}">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h4 class="card-title">
+                            {{$product->display_name}}
+                        </h4>
+                        <div class="row">
+                            <div class="col-12">
+                                <ul class="bhm-list">
+                                    <li>
+                                        <a href="{{route('products.stats', ['id' => $product->id])}}">
+                                            24HR Stats
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('products.history', ['id' => $product->id])}}">
+                                            Trade History
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="{{route('products.order-book', ['id' => $product->id])}}">
+                                            Order Book
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#!">
+                                            Ticker
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
         <div class="row">
             <div class="col-12">
                 <div class="text-right">
