a3a0208c0b0384b4464e010166ad82c4982ed628
LT <support@teran.tech>
2022-02-22 11:55:27 -0700

    Added Nunito font to project. No longer pulling from Google CDN.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,78 +8,7 @@
     <title>Bahamut</title>
 
     <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" />
     <link href="{{asset('css/bahamut.css')}}" rel="stylesheet" />
-
-    <!-- Styles -->
-    <style>
-        html,
-        body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .sub-title {
-            font-size: 15%;
-            display: block;
-            margin-top: -15px;
-        }
-
-        .links>a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-
-        @media only screen and (max-width: 496px) {
-            .links>a {
-                font-size: 16px;
-                padding: 0;
-                display: block;
-                margin-bottom: 15px;
-            }
-        }
-    </style>
 </head>
 
 <body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,106 @@
-@extends('layouts.app')
-@section('content')
-<div class="flex-center position-ref full-height">
-   <div class="content">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-      <div class="title m-b-md">
-         Bahamut <small class="sub-title">powered by Coinbase</small>
-      </div>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bahamut</title>
 
-      <div class="links">
-         <a href="{{route('exchange')}}">Exchange</a>
-         <a href="{{route('products')}}">Products</a>
-         <a href="#!">History</a>
-         <a href="{{route('portfolios')}}">Portfolios</a>
-         <a href="https://github.com/lteran9/Bahamut">GitHub</a>
-      </div>
+    <!-- Styles -->
+    <style>
+        @font-face{
+            font-family: 'NunitoVariableFont';
+            src: url('/fonts/NunitoVariableFont.ttf');
+        }
 
-   </div>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'NunitoVariableFont', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .sub-title {
+            font-size: 15%;
+            display: block;
+            margin-top: -15px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+
+        @media only screen and (max-width: 496px) {
+            .links>a {
+                font-size: 16px;
+                padding: 0;
+                display: block;
+                margin-bottom: 15px;
+            }
+        }
+    </style>
+</head>
+
+<body>
+   <div class="flex-center position-ref full-height">
+    <div class="content">
+
+        <div class="title m-b-md">
+            Bahamut <small class="sub-title">powered by Coinbase</small>
+        </div>
+
+        <div class="links">
+            <a href="{{route('exchange')}}">Exchange</a>
+            <a href="#!">History</a>
+            <a href="{{route('portfolios')}}">Portfolios</a>
+            <a href="#!">Settings</a>
+            <a href="https://github.com/lteran9/Bahamut">GitHub</a>
+        </div>
+
+    </div>
 </div>
-@endsection
+
+</body>
+
+</html>
